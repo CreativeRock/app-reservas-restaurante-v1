9047e4c465123e0f2d3a77a2f12c71a798fb5055
CreativeRock <ccalelc2@gmail.com>
2025-10-01 18:28:47 -0600

    Configuraciónes Iniciales App Reservas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,91 @@
 import { Routes } from '@angular/router';
+import { clienteAuthGuard } from './core/guards/cliente-auth-guard';
+import { staffAuthGuard } from './core/guards/staff-auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //Landing Page
+  {
+    path: '',
+    loadComponent: () => import('./features/landing/components/landing-page/landing-page')
+      .then(componente => componente.LandingPage)
+  },
+
+  //Autenticación de clientes
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/components/cliente-login/cliente-login')
+      .then(componente => componente.ClienteLogin)
+  },
+  {
+    path: 'registro',
+    loadComponent: () => import('./features/auth/components/cliente-register/cliente-register')
+      .then(componente => componente.ClienteRegister)
+  },
+  {
+    path: 'disponibiliad',
+    loadComponent: () => import('./features/reservacion/components/disponibilidad/disponibilidad')
+      .then(componente => componente.Disponibilidad)
+  },
+  {
+    path: 'mesas',
+    loadComponent: () => import('./features/reservacion/components/mesas-galeria/mesas-galeria')
+      .then(componente => componente.MesasGaleria)
+  },
+  {
+    path: 'reservacion/:tableId',
+    loadComponent: () => import('./features/reservacion/components/reservacion-detalles/reservacion-detalles')
+      .then(componente => componente.ReservacionDetalles)
+  },
+  {
+    path: 'reservacion/confirmacion/:reservaId',
+    loadComponent: () => import('./features/reservacion/components/reservacion-confirmacion/reservacion-confirmacion')
+      .then(componente => componente.ReservacionConfirmacion)
+  },
+  {
+    path: 'cliente',
+    canActivate: [clienteAuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/cliente/components/dashboard/dashboard')
+          .then(componente => componente.Dashboard)
+      },
+      {
+        path: 'mis-reservas',
+        loadComponent: () => import('./features/cliente/components/mi-reservacion/mi-reservacion')
+          .then(componente => componente.MiReservacion)
+      },
+      {
+        path: 'perfil',
+        loadComponent: () => import('./features/cliente/components/profile/profile')
+          .then(componente => componente.Profile)
+      }
+    ]
+  },
+  {
+    path: 'staff',
+    canActivate: [staffAuthGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/staff/components/dashboard/dashboard')
+          .then(componente => componente.Dashboard)
+      },
+      {
+        path: 'reservas',
+        loadComponent: () => import('./features/staff/components/reservas/reservas')
+          .then(componente => componente.Reservas)
+      }
+    ]
+  },
+  {
+    path: 'staff/login',
+    loadComponent: () => import('./features/staff/components/staff-login/staff-login')
+    .then(componente => componente.StaffLogin)
+  },
+  //Ruta por defecto
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
